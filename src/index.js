43d9a1f49a1afd90e876db01987471e095ed797b
Kaitlyn McGoldrick <mcgoldrick.kaitlyn@gmail.com>
2018-04-08 22:50:51 -0400

    Remove serviceWorker for gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter } from 'react-router-dom'
 
@@ -10,4 +9,3 @@ ReactDOM.render((
         <App />
     </BrowserRouter>
 ), document.getElementById('root'));
-registerServiceWorker();
